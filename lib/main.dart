74308fc101614ae64ba83670e88ce39cc200a791
Parth Soneji <parthsoneji.softrefine@gmail.com>
2023-05-09 18:35:05 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:realstateapp/Screens/Home/NavigationBar.dart';
+import 'package:realstateapp/Screens/SplashScreen/SplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavigationScreen(),
+      home: SplashScreen(),
     );
   }
 }
